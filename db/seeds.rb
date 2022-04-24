e930b03ded197035ba7057026f3cf3236725e1dc
Sarah Haubert <sarah.haubert@hotmail.com>
2022-04-24 16:54:24 -0600

    add courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+20.times do 
+    Course.create(title:Faker::Mountain.name, body:Faker::Quote.yoda)
+end
